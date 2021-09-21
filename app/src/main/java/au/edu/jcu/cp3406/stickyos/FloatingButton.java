1d681248ee2c8d8e55972a954cc589a3caa665e6
Corey Jamieson <corey.jamieson@my.jcu.edu.au>
2021-09-21 14:54:16 +1000

    Corey first commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/stickyos/FloatingButton.java
@@ -0,0 +1,14 @@
+package au.edu.jcu.cp3406.stickyos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FloatingButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_button2);
+    }
+}
