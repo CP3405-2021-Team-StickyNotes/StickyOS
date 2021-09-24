c6747d7f264357ba45ac7e20f4eefd7d7859c609
yuvibernstein <yuval.bernstein@my.jcu.edu.au>
2021-09-25 00:16:00 +1000

    First Commit - add contact homepage

---
+++ app/src/main/java/au/edu/jcu/cp3406/stickyos/AddContact.java
@@ -0,0 +1,13 @@
+package au.edu.jcu.cp3406.stickyos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
--- app/src/main/java/au/edu/jcu/cp3406/stickyos/Contacts.java
+++ app/src/main/java/au/edu/jcu/cp3406/stickyos/Contacts.java
@@ -1,34 +1,35 @@
 package au.edu.jcu.cp3406.stickyos;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+public class Contacts extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+    ImageButton androidImageButton;
     private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contacts);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        androidImageButton = (ImageButton) findViewById(R.id.addContact);
+        androidImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                openActivity3();
             }
         });
     }
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, Contacts.class);
+    public void openActivity3() {
+        Intent intent = new Intent(this, AddContact.class);
         startActivity(intent);
     }
 }
-
-
