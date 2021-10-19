8f745bd19e76bf27aede9dc2cd1a386241f50ae0
Corey Jamieson <corey.jamieson@my.jcu.edu.au>
2021-10-19 12:29:28 +1000

    Add FAB

--- app/src/main/java/au/edu/jcu/cp3406/stickyos/FloatingButton.java
+++ app/src/main/java/au/edu/jcu/cp3406/stickyos/FloatingButton.java
@@ -3,6 +3,11 @@ package au.edu.jcu.cp3406.stickyos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FloatingButton extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class FloatingButton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_button2);
+
+        FloatingActionButton fab = findViewById(R.id.help_button);
+
+        fab.setOnClickListener(view -> Toast.makeText(FloatingButton.this, "Test", Toast.LENGTH_SHORT).show());
     }
+
 }
