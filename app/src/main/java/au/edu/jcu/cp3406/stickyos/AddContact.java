b85a4cdf83cd18d60a7782bfc407820ee5ab235f
matthewhall98 <matthew.hall1@my.jcu.edu.au>
2021-10-26 08:02:42 +1000

    Change formmating for contacts app

--- app/src/main/java/au/edu/jcu/cp3406/stickyos/AddContact.java
+++ app/src/main/java/au/edu/jcu/cp3406/stickyos/AddContact.java
@@ -3,6 +3,8 @@ package au.edu.jcu.cp3406.stickyos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddContact extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class AddContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        Button submitButton = (Button) findViewById(R.id.buttonSubmit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Button cancelButton = (Button) findViewById(R.id.buttonReset);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
